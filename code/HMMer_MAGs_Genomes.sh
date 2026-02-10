65222c7831536a96164d31802b55f9bbd7d61bb7
STabuteau <119591618+STabuteau@users.noreply.github.com>
2026-02-10 12:00:03 +0100

    HMMer_MAGs_Genomes.sh

--- code/HMMer_MAGs_Genomes.sh
+++ code/HMMer_MAGs_Genomes.sh
@@ -18,7 +18,7 @@ do
    sort -k4,4 | \
    awk 'BEGIN{FS="\t"; OFS="\t"}{if(($19-$18+1)/$6>=0.7){print $0,($19-$18+1),($19-$18+1)/$6}}' | awk 'BEGIN{FS="\t"; OFS="\t"}{print $1,$3,$4,$6,$8,$13,$16,$17,$18,$19,$NF}' | \
    sed 's/,/\./g' > ${samplename}_parse_file.tmp
-   Rscript merge_files.R ${samplename}_parse_file7.tmp HMMs_metadata.tsv ${samplename}_parse_merge.tsv
+   Rscript merge_files.R ${samplename}_parse_file.tmp HMMs_metadata.tsv ${samplename}_parse_merge.tsv
    awk -F '\t' '{if($5>=$NF) {print $0}}' ${samplename}_parse_merge.tsv | awk '$5 > max[$3] { max[$3] = $5; m[$3] = $0 } END { for (i in m) { print m[i] } }' > ${samplename}_parse_merge_bitscore.tsv
    awk -F "\t" '{print $1}' ${samplename}_parse_merge_bitscore.tsv | sed 's/$/\t1/' | sort | sed '1iHMMs_ID\tCount' | awk 'NR==1{print;next} {for (i=2;i<=NF;i++) {a[$1][i]+=$i}} END{ for (j in a) {s=j; for (i=2;i<=NF;i++) {s=s" "a[j][i]}; print s}}' | sed 's/ /\t/' | sed 's/$/\t'${samplename}'/' | sed '1d' | sed '1iHMMs_ID\tCount\tGenomes' > ${samplename}_parse_merge_bitscore_Count.tsv
 done
@@ -26,4 +26,5 @@ rm -f *_parse_file.tmp
 rm -f *_parse_merge.tsv
 
 cat *_parse_merge_bitscore_Count.tsv | grep -v 'Count' | sed '1iHMMs_ID\tCount\tGenomes' > Genomes_parse_merge_bitscore_Count.tsv
+
 
